ff4c2dbb592845af883c256d39d198edba8147fe
i2plover <ben@bentheman96.com>
2015-09-05 22:12:26 -0400

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+sudo apt-get update && upgrade
+sudo apt-get install software-properties-common python-software-properties
+sudo apt-get remove apache2*
 sudo apt-add-repository ppa:i2p-maintainers/i2p
 sudo apt-get update
-sudo apt-get install i2p
+sudo apt-get install i2p lighttpd
 dpkg-reconfigure i2p
